dd34c6f620efc4841ac2d46e0078adbc089a64bd
Dni <divixed12@gmail.com>
2020-05-13 11:03:03 +0200

    fixed

--- data_handler.py
+++ data_handler.py
@@ -7,21 +7,24 @@ def get_card_status(status_id):
     :param status_id:
     :return: str
     """
-    statuses = persistence.get_statuses()
+    # statuses = persistence.get_statuses()
+    statuses = persistence.get_table_data(table="statuses")
     return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
-
 
 def get_boards():
     """
     Gather all boards
     :return:
     """
-    return persistence.get_boards(force=True)
+
+    # return persistence.get_boards(force=True)
+    return persistence.get_table_data(table="boards")
 
 
 def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
+    # persistence.clear_cache()
+    # all_cards = persistence.get_cards()
+    all_cards = persistence.get_table_data(table="cards")
     matching_cards = []
     for card in all_cards:
         if card['board_id'] == str(board_id):
